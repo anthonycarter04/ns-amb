7b54c7f9290c12f49a255b2d0a74e94d8cd42baa
Anthony Carter <anthony.carter04@gmail.com>
2015-12-02 14:17:47 -0800

    Add initial file

---
+++ Documents/workspace/NS Scheduled/statement-generator.js
@@ -0,0 +1,15 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       02 Dec 2015     carter
+ *
+ */
+
+/**
+ * @param {String} type Context Types: scheduled, ondemand, userinterface, aborted, skipped
+ * @returns {Void}
+ */
+function sGen(type) {
+	
+}
