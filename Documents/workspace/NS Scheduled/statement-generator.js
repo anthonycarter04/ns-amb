f456c16e99ddb0d90b95953f93622b8f3b309d34
Anthony Carter <anthony.carter04@gmail.com>
2015-12-02 16:12:40 -0800

    Update two

--- Documents/workspace/NS Scheduled/statement-generator.js
+++ Documents/workspace/NS Scheduled/statement-generator.js
@@ -12,4 +12,56 @@
  */
 function sGen(type) {
 	
+	var cust = getCustomers();
+	
+	if (cust && cust != '') {
+		
+		for (var i=0; i<1 /*cust.length*/; i++) {
+			
+			var custId = cust[i];
+			if (custId && custId != '') {
+				
+				var filter = [new nlobjSearchFilter('entity',null, 'anyof', custId)];
+				
+				var invs = getInvoices(filter);
+				
+				
+			}
+			
+			
+			var over = checkUsage('1000');
+			if (over && over == 'Yes') {
+				break;
+			}
+		}
+		
+	}
+	
+}
+
+function getInvoices(filter) {
+	
+	var searchResults = nlapiSearchRecord('transaction', 'customsearch_sgen_invoices', null, null);
+	
+	
+}
+
+function getCustomers() {
+	
+	var searchResults = nlapiSearchRecord('transaction', 'customsearch_sgen_customers', null, null);
+	return searchResults;
+	
+}
+
+function checkUsage(units) {
+	var context = nlapiGetContext();
+	var remUsage = context.getRemainingUsage();
+	
+	if (remUsage && remUsage < units) {
+		nlapiScheduleScript(context.getScriptId(), context.getDeploymentId());
+		return 'Yes';
+	} else {
+		return 'No';
+	}
+	
 }
